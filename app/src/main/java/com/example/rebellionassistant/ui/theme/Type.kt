2604c8b2966e4d3909fe8d66f1558341ed2e2e04
Geoff81 <geoff.dessain@gmail.com>
2025-05-14 20:43:30 +0200

    Create Type.kt

---
+++ app/src/main/java/com/example/rebellionassistant/ui/theme/Type.kt
@@ -0,0 +1,15 @@
+package com.example.rebellionassistant.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontSize = 16.sp
+    )
+    // Other default text styles to override
+)
